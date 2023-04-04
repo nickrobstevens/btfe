e3240955530ba159f17e9c7365b6ccfa01f25964
nickstevens <nick@nrstevens.com>
2023-04-05 00:42:43 +1000

    Stubbing unit tests for APIs

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Post } from '../models/post.model';
-import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
