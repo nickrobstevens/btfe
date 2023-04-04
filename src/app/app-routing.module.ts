6431c32fa2d4ad081f1553a29c59834e7306212a
nickstevens <nick@nrstevens.com>
2023-04-04 22:11:32 +1000

    Scaffolding: created pages, components and services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
+import { UserComponent } from './pages/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'user/:userID', component: UserComponent },
+  { path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './pages/index/index.component';
+import { UserComponent } from './pages/user/user.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    UserComponent,
+    HeaderComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
