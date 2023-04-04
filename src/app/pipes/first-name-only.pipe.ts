36c61c4bf3748f7a4be27e88ec0a87f7ac03dabf
nickstevens <nick@nrstevens.com>
2023-04-04 23:49:08 +1000

    Names: better handling for first name detection

--- src/app/pipes/first-name-only.pipe.ts
+++ src/app/pipes/first-name-only.pipe.ts
@@ -6,7 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FirstNameOnlyPipe implements PipeTransform {
 
   transform(value: string): string {
-    return value.substring(0, value.indexOf(' '));
+
+    const titles = ['Mr.', 'Mrs.', 'Ms.', "Miss.", "Dr."];
+    const nameParts = value.split(' ');
+
+    // Filter out the titles
+    return (titles.includes(nameParts[0])) ? nameParts[1] : nameParts[0];
+
   }
 
 }
