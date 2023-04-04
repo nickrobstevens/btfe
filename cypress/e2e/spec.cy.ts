f8c2173e7845042b7ae58eb572a4309051283cec
nickstevens <nick@nrstevens.com>
2023-04-05 00:51:35 +1000

    UI testing with cypress

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,41 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
+describe('App loads', () => {
+
+  it('Shows the landing page', () => {
     cy.visit('/')
-    cy.contains('app is running!')
+    cy.contains('Landing!')
   })
+
+
+  it('Has a list of people', () => {
+    cy.visit('/')
+    cy.get('header li').should('have.length', 10)
+  })
+
+
+  it('Loads a user who exists', () => {
+    cy.visit('/')
+    cy.get('header a').first().click()
+    cy.contains('Posts from Leanne Graham:')
+  })
+
+  it('Loads a users posts', () => {
+    cy.visit('/')
+    cy.get('header a').first().click()
+    cy.get('.user-post').should('have.length.at.least', 1)
+  })
+
+  it('Loads all user posts', () => {
+    cy.visit('/')
+    cy.get('header a').first().click()
+    cy.get('.user-actions .btn').click();
+    cy.get('.user-post').should('have.length.above', 3)
+  })
+
+  it('Can expand comments on a post', () => {
+    cy.visit('/')
+    cy.get('header a').first().click()
+    cy.get('.user-post .btn').first().click();
+    cy.get('.post-comment').should('have.length.at.least', 1)
+  })
+
 })
